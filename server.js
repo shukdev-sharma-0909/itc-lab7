5905e542eee080c08735d6ec00ab74c5a42c3471
shukdev-sharma-0909 <shukdev.usa@gmail.com>
2024-11-29 22:40:15 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,28 +13,28 @@ const publicServedFilesPath = path.join(__dirname, 'public');
 server.use(express.static(publicServedFilesPath));
 
 // Random number route
-server.get('/do_a_random', (req, res) => {
+server.get('/generate_random', (req, res) => {
   res.send(`Your number is: ${Math.floor(Math.random() * 100) + 1}`);
 });
 
 // Mad Lib form submission route
 server.post('/submit', (req, res) => {
-  const { adjective, noun, verb, adverb, number } = req.body;
+  const { adjective, noun, verb, place, pluralNoun } = req.body;
 
-  if (!adjective || !noun || !verb || !adverb || !number) {
+  if (!adjective || !noun || !verb || !place || !pluralNoun) {
     res.send(`
       <h1>Submission Failed</h1>
       <p>Please fill out all fields.</p>
-      <a href="index.html">Go Back to Form</a>
+      <a href="/ITC505/lab-7/index.html">Go Back to Form</a>
     `);
     return;
   }
 
-  const madLib = `Today I saw a ${number} ${adjective} ${noun}s, and they all decided to ${verb} ${adverb}!`;
+  const madLib = `Today I saw a ${place} ${adjective} ${noun}s, and they all decided to ${verb} ${pluralNoun}!`;
   res.send(`
     <h1>Submission Successful</h1>
     <p>${madLib}</p>
-    <a href="index.html">Go Back to Form</a>
+    <a href="/ITC505/lab-7/index.html">Go Back to Form</a>
   `);
 });
 
